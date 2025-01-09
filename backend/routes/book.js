8872bb44f3db2001d983ad1181905f2e9f4b007f
sahar225 <sgeforce123@gmail.com>
2025-01-09 14:14:17 -0500

    solve internal server error in  add book module

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -29,7 +29,7 @@ router.post("/add-book", authenticateToken, async (req, res) => {
     res.status(200).json({mesage: "Book added successfully"}); 
  } catch (error) {
     console.log(error)   
-    res.status(500).json({mesage: "Internal"});  
+    res.status(500).json({mesage: "Internal server error"});  
                
     }
 });
